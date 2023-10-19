861a5ff377ae835f4a8b5f64fd7505354d09d96a
Jorge Reyes <92123185+JorgeArt81@users.noreply.github.com>
2023-10-18 21:32:29 -0500

    feat: migrated components

--- src/components/Proyecto/Proyecto.jsx
+++ src/components/Proyecto/Proyecto.jsx
@@ -1,22 +1,25 @@
-import React from "react";
-import "../../assets/css/proyecto.css";
+import PropTypes from 'prop-types';
+
+import './proyecto.css';
 
 export const Proyecto = ({ services }) => {
-
-
   return (
-    <div className="contain-proyecto">
-      {services.map((service) => (
-        <div className="contain-proyecto-cont">
-          <div className="high-container" key={service.id}>
-            <h3 className="high-container-title">{service.nombre}</h3>
-            <h4 className="high-container-capsula">{service.categoria}</h4>
+    <div className='contain-proyecto'>
+      {services.map(service => (
+        <div key={service.id} className='contain-proyecto-cont'>
+          <div className='high-container' key={service.id}>
+            <h3 className='high-container-title'>{service.nombre}</h3>
+            <h4 className='high-container-capsula'>{service.categoria}</h4>
           </div>
-          <div className="contain-proyecto_low">
-            <img src={service.imagen} alt="confioPilar" />
+          <div className='contain-proyecto_low'>
+            <img src={service.imagen} alt='confioPilar' />
           </div>
-          </div>
+        </div>
       ))}
     </div>
   );
+};
+
+Proyecto.propTypes = {
+  services: PropTypes.func.isRequired, // Mark 'change' as a required function prop
 };
--- src/components/Redes/Redes.jsx
+++ src/components/Redes/Redes.jsx
@@ -1,28 +1,44 @@
-import React from "react";
-import '../../assets/css/redes.css'
-import GitHubIcon from '@mui/icons-material/GitHub';
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import LanguageIcon from '@mui/icons-material/Language';
-import BusinessCenterIcon from '@mui/icons-material/BusinessCenter';
-import InstagramIcon from '@mui/icons-material/Instagram';
 import { Link } from 'react-router-dom';
 
+import BusinessCenterIcon from '@mui/icons-material/BusinessCenter';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import LanguageIcon from '@mui/icons-material/Language';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+
+import './redes.css';
+
 export const Redes = () => {
-    return(
-        <footer className="footer_contain">
-            <div className="footer_contain-txt">
-                <h3>Copyright © 2023 All rights reserved</h3>
-            </div>
-            <div className="footer_contain-logos">
-                <Link href="https://github.com/AndresCarlosamaDev"><GitHubIcon sx={{ color: "white", fontSize: 35 }} target="blank" /></Link>
-                <Link href="https://www.linkedin.com/in/andrescarlosama/"><LinkedInIcon sx={{ color: "white", fontSize: 35 }} target="blank" /></Link>
-                <Link href="#"><LanguageIcon sx={{ color: "white", fontSize: 35 }} target="blank" /></Link>
-                <Link href="https://www.behance.net/Andres_7k"><BusinessCenterIcon sx={{ color: "white", fontSize: 35 }} target="blank" /></Link>
-                <Link href="https://www.instagram.com/andreus_eatos/"><InstagramIcon sx={{ color: "white", fontSize: 35 }} target="blank" /></Link>
-            </div>
-            <div className="footer_contain-txt">
-                <h3>Desarrollado por <b>Andrés Carlosama</b></h3>
-            </div>
-        </footer>
-    )
-}
+  return (
+    <footer className='footer_contain'>
+      <div className='footer_contain-txt'>
+        <h3>Copyright © 2023 All rights reserved</h3>
+      </div>
+      <div className='footer_contain-logos'>
+        <Link href='https://github.com/AndresCarlosamaDev'>
+          <GitHubIcon sx={{ color: 'white', fontSize: 35 }} target='blank' />
+        </Link>
+        <Link href='https://www.linkedin.com/in/andrescarlosama/'>
+          <LinkedInIcon sx={{ color: 'white', fontSize: 35 }} target='blank' />
+        </Link>
+        <Link href='#'>
+          <LanguageIcon sx={{ color: 'white', fontSize: 35 }} target='blank' />
+        </Link>
+        <Link href='https://www.behance.net/Andres_7k'>
+          <BusinessCenterIcon
+            sx={{ color: 'white', fontSize: 35 }}
+            target='blank'
+          />
+        </Link>
+        <Link href='https://www.instagram.com/andreus_eatos/'>
+          <InstagramIcon sx={{ color: 'white', fontSize: 35 }} target='blank' />
+        </Link>
+      </div>
+      <div className='footer_contain-txt'>
+        <h3>
+          Desarrollado por <b>Andrés Carlosama</b>
+        </h3>
+      </div>
+    </footer>
+  );
+};
