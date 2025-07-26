266e5bacf68cc95b7718c3a1763baee99d7618dc
AndresCarlosamaDev <andrescarlosdev@gmail.com>
2025-07-26 13:38:36 -0500

    Update Google Search Console

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -6,7 +6,7 @@ export const Banner = () => {
       <h3 className='contain-banner-h3'>Mi nombre es</h3>
       <h1 className='contain-banner-h1_A'>Andrés</h1>
       <h1 className='contain-banner-h1_B'>Carlosama</h1>
-      <h4 className='contain-banner-h4'>Diseño Gráfico / UX UI</h4>
+      <h4 className='contain-banner-h4'>Diseño Gráfico / UX UI / Desarrollo Web</h4>
     </div>
   );
 };
